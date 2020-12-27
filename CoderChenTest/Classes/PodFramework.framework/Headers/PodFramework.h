eb00de729566c31687ea9c345efbc770c5d1c940
liangliang <cll7781@163.com>
2020-12-27 12:26:16 +0800

    增加framework

---
+++ CoderChenTest/Classes/PodFramework.framework/Headers/PodFramework.h
@@ -0,0 +1,19 @@
+//
+//  PodFramework.h
+//  PodFramework
+//
+//  Created by 陈良良 on 2020/12/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodFramework.
+FOUNDATION_EXPORT double PodFrameworkVersionNumber;
+
+//! Project version string for PodFramework.
+FOUNDATION_EXPORT const unsigned char PodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodFramework/PublicHeader.h>
+
+#import <PodFramework/PrintMyText.h>
+
---
+++ CoderChenTest/Classes/PodFramework.framework/Headers/PrintMyText.h
@@ -0,0 +1,18 @@
+//
+//  PrintMyText.h
+//  PodFramework
+//
+//  Created by 陈良良 on 2020/12/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PrintMyText : NSObject
+
++(void) pppppppppp;
+
+@end
+
+NS_ASSUME_NONNULL_END
